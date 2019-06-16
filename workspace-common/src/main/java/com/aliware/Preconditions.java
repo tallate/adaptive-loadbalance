be9c15466ee47e6825ad0f6ac2e6e88402b5041c
tallate <787985369@qq.com>
2019-06-16 23:34:53 +0800

    [fix]无效的注解

--- workspace-common/src/main/java/com/aliware/Preconditions.java
+++ workspace-common/src/main/java/com/aliware/Preconditions.java
@@ -1,10 +1,9 @@
 package com.aliware;
 
-import com.sun.istack.internal.Nullable;
-
 /**
  * 参数预处理
- * @see Guava - Preconditions
+ *
+ * @see "Guava - Preconditions"
  */
 public final class Preconditions {
 
@@ -17,15 +16,15 @@ public final class Preconditions {
         }
     }
 
-    public static void checkArgument(boolean expression, @Nullable Object errorMessage) {
+    public static void checkArgument(boolean expression, Object errorMessage) {
         if (!expression) {
             throw new IllegalArgumentException(String.valueOf(errorMessage));
         }
     }
 
     public static void checkArgument(boolean expression,
-            @Nullable String errorMessageTemplate,
-            @Nullable Object... errorMessageArgs) {
+                                     String errorMessageTemplate,
+                                     Object... errorMessageArgs) {
         if (!expression) {
             throw new IllegalArgumentException(format(errorMessageTemplate, errorMessageArgs));
         }
@@ -37,15 +36,15 @@ public final class Preconditions {
         }
     }
 
-    public static void checkState(boolean expression, @Nullable Object errorMessage) {
+    public static void checkState(boolean expression, Object errorMessage) {
         if (!expression) {
             throw new IllegalStateException(String.valueOf(errorMessage));
         }
     }
 
     public static void checkState(boolean expression,
-            @Nullable String errorMessageTemplate,
-            @Nullable Object... errorMessageArgs) {
+                                  String errorMessageTemplate,
+                                  Object... errorMessageArgs) {
         if (!expression) {
             throw new IllegalStateException(format(errorMessageTemplate, errorMessageArgs));
         }
@@ -58,7 +57,7 @@ public final class Preconditions {
         return reference;
     }
 
-    public static <T> T checkNotNull(T reference, @Nullable Object errorMessage) {
+    public static <T> T checkNotNull(T reference, Object errorMessage) {
         if (reference == null) {
             throw new NullPointerException(String.valueOf(errorMessage));
         }
@@ -66,8 +65,8 @@ public final class Preconditions {
     }
 
     public static <T> T checkNotNull(T reference,
-            @Nullable String errorMessageTemplate,
-            @Nullable Object... errorMessageArgs) {
+                                     String errorMessageTemplate,
+                                     Object... errorMessageArgs) {
         if (reference == null) {
             // If either of these parameters is null, the right thing happens anyway
             throw new NullPointerException(format(errorMessageTemplate, errorMessageArgs));
@@ -80,7 +79,7 @@ public final class Preconditions {
     }
 
     public static int checkElementIndex(
-            int index, int size, @Nullable String desc) {
+            int index, int size, String desc) {
         // Carefully optimized for execution by hotspot (explanatory comment above)
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException(badElementIndex(index, size, desc));
@@ -102,7 +101,7 @@ public final class Preconditions {
         return checkPositionIndex(index, size, "index");
     }
 
-    public static int checkPositionIndex(int index, int size, @Nullable String desc) {
+    public static int checkPositionIndex(int index, int size, String desc) {
         // Carefully optimized for execution by hotspot (explanatory comment above)
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException(badPositionIndex(index, size, desc));
@@ -138,7 +137,7 @@ public final class Preconditions {
         return format("end index (%s) must not be less than start index (%s)", end, start);
     }
 
-    static String format(String template, @Nullable Object... args) {
+    static String format(String template, Object... args) {
         // null -> "null"
         template = String.valueOf(template);
 
