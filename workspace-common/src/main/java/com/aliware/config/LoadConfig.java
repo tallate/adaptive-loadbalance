574fbfb378ea731d37d799c70342dab4bd098c45
tallate <787985369@qq.com>
2019-06-23 22:51:14 +0800

    第2版-调个参

--- workspace-common/src/main/java/com/aliware/config/LoadConfig.java
+++ workspace-common/src/main/java/com/aliware/config/LoadConfig.java
@@ -8,8 +8,8 @@ public class LoadConfig {
      * 负载均衡算法切换阈值
      * 超出容许处理能力到一定程度后，切换负载均衡算法
      */
-    public static final double LOAD_THRESHOLD = 0.05;
+    public static final double LOAD_THRESHOLD = 0.01;
 
-    public static final double EXTEND_FACTOR = 5;
+    public static final double EXTEND_FACTOR = 10;
 
 }
